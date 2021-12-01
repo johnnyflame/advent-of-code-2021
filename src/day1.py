210a5b668a8b6f3dd937aba86a2dd4ed419ecd00
Johnny Lee <johnny.lee@ocado.com>
2021-12-01 14:41:43 +0100

    day 1 part 2

--- src/day1.py
+++ src/day1.py
@@ -9,5 +9,17 @@ def count_depth_increment(data):
     return increments
 
 
+def count_depth_increment_sum_of_three(data):
+    increments = 0
+    prev_sum = data[0] + data[1] + data[2]
+    for day1, day2, day3 in zip(data, data[1:], data[2:]):
+        curr_sum = day1 + day2 + day3
+        if curr_sum > prev_sum:
+            increments += 1
+        prev_sum = curr_sum
+    return increments
+
+
 if __name__ == "__main__":
     print(f"part 1 answer {count_depth_increment(numbers)}")
+    print(f"part 2 answer {count_depth_increment_sum_of_three(numbers)}")
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,4 +1,4 @@
-from src.day1 import count_depth_increment
+from src.day1 import count_depth_increment, count_depth_increment_sum_of_three
 
 
 def test_count_increment():
@@ -16,3 +16,20 @@ def test_count_increment():
 """
     test_input = [int(line) for line in raw_data.splitlines() if line]
     assert count_depth_increment(test_input) == 7
+
+
+def test_count_increment_sum_of_three():
+    raw_data = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+"""
+    test_input = [int(line) for line in raw_data.splitlines() if line]
+    assert count_depth_increment_sum_of_three(test_input) == 5
