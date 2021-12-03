0bf79011ff8e2defd8b3d82dccd0521a43f667fb
Johnny Lee <johnny.lee@ocado.com>
2021-12-03 11:07:31 +0100

    refactor of day 1 test cases to remove duplicated test data

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,8 +1,6 @@
 from src.day1 import count_depth_increment, count_depth_increment_sum_of_three
 
-
-def test_count_increment():
-    raw_data = """
+raw_data = """
     199
     200
     208
@@ -14,22 +12,12 @@ def test_count_increment():
     260
     263
 """
-    test_input = [int(line) for line in raw_data.splitlines() if line]
+test_input = [int(line) for line in raw_data.splitlines() if line]
+
+
+def test_count_increment():
     assert count_depth_increment(test_input) == 7
 
 
 def test_count_increment_sum_of_three():
-    raw_data = """
-    199
-    200
-    208
-    210
-    200
-    207
-    240
-    269
-    260
-    263
-"""
-    test_input = [int(line) for line in raw_data.splitlines() if line]
     assert count_depth_increment_sum_of_three(test_input) == 5
