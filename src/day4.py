d77a127f6b8fb24e75a6cef6b5d389175b505c5b
johnnyflame <johnnyflamelee@gmail.com>
2021-12-04 18:18:07 +0100

    Day 4 solution. Part 1 working

---
+++ src/day4.py
@@ -0,0 +1,110 @@
+from aocd import get_data
+
+
+def bingo(data):
+    numbers, boards = parse_input(data)
+    winner, marking_sheet = get_winner(numbers, boards)
+    return compute_score(winner, marking_sheet)
+
+
+def get_last_winner_score(data):
+    numbers, boards = parse_input(data)
+    winners, marking_sheets = get_winners(numbers, boards)
+
+    return compute_score(winners[-1], marking_sheets[-1])
+
+
+def compute_score(winner, marking_sheet):
+    accum = 0
+    multiplier = 0
+    for row in range(len(marking_sheet)):
+        for col in range(len(marking_sheet)):
+            if marking_sheet[row][col] == 0:
+                accum += winner[row][col]
+            elif marking_sheet[row][col] == 2:
+                multiplier = winner[row][col]
+
+    return accum * multiplier
+
+
+def get_winners(numbers, boards):
+    rows = len(boards[0])
+    cols = len(boards[0][0])
+    marking_sheets = [[[0] * cols for _ in range(rows)] for _ in range(len(boards))]
+
+    winners = []
+    winning_marking_sheets = []
+    # iterate through all boards, for each row and column
+    for number in numbers:
+        for idx, board in enumerate(boards):
+            marking_sheet = marking_sheets[idx]
+            for row in range(rows):
+                for col in range(cols):
+                    if board[row][col] == number:
+                        marking_sheet[row][col] = 1
+
+                    col_vals = [row[col] for row in marking_sheet]
+                    if set(col_vals) == {1}:
+                        marking_sheet[row][col] = 2
+                        winners.append(board)
+                        winning_marking_sheets.append(marking_sheet)
+                        break
+
+                    row_vals = marking_sheet[row]
+                    if set(row_vals) == {1}:
+                        marking_sheet[row][col] = 2
+                        winners.append(board)
+                        winning_marking_sheets.append(marking_sheet)
+                        break
+
+        if len(winners) == len(boards):
+            break
+    return winners, winning_marking_sheets
+
+
+def get_winner(numbers, boards):
+    rows = len(boards[0])
+    cols = len(boards[0][0])
+    marking_sheets = [[[0] * cols for _ in range(rows)] for _ in range(len(boards))]
+
+    # iterate through all boards, for each row and column
+    for number in numbers:
+        for idx, board in enumerate(boards):
+            marking_sheet = marking_sheets[idx]
+            for row in range(rows):
+                for col in range(cols):
+                    if board[row][col] == number:
+                        marking_sheet[row][col] = 1
+
+                    col_vals = [row[col] for row in marking_sheet]
+                    if set(col_vals) == {1}:
+                        marking_sheet[row][col] = 2
+                        return (board, marking_sheet)
+
+                    row_vals = marking_sheet[row]
+                    if set(row_vals) == {1}:
+                        marking_sheet[row][col] = 2
+                        return (board, marking_sheet)
+
+
+def parse_input(data):
+    data = data.splitlines()
+    numbers = [int(num) for num in data[0].split(",")]
+
+    boards = []
+    board = []
+    for line in data[2:]:
+        if line == "":
+            boards.append(board)
+            board = []
+        else:
+            board.append([int(num) for num in line.split(" ") if num])
+    boards.append(board)
+
+    return numbers, boards
+
+
+if __name__ == "__main__":
+    data = get_data(day=4)
+    print(f"part 1 answer: {bingo(data)}")
+    print(f"part 2 answer: {get_last_winner_score(data)}")
